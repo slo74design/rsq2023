77185a648531999f83d262aa1fab6fb1996e47af
slo74design <salvologiudice@gmail.com>
2023-07-31 16:19:12 +0300

    headlineAbout1

--- components/public/About-Historia.js
+++ components/public/About-Historia.js
@@ -49,7 +49,7 @@ const AboutHistoria = () => {
                         <h3
                             className={`${outfit.className} text-xl lg:text-3xl leading-6 font-extrabold text-rsq-100`}
                         >
-                            RemoteSquid, la agencia web estona con mentalidad
+                            RemoteSquid, la agencia web OÜ con mentalidad
                             española
                         </h3>
                         <p
