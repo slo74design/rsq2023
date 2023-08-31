435fe07e96e385189a797859a0474b7d775003e6
Slo74design <salvologiudice@gmail.com>
2023-08-31 17:11:43 +0300

    sitemap

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+//     reactStrictMode: true,
+//     images: {
+//         domains: ["res.cloudinary.com"],
+//     },
+// };
+// module.exports = nextConfig;
+module.exports = {
     reactStrictMode: true,
     images: {
         domains: ["res.cloudinary.com"],
     },
+    async rewrites() {
+        return [
+            {
+                source: "/sitemap.xml",
+                destination: "/api/sitemap",
+            },
+        ];
+    },
 };
 
-module.exports = nextConfig;
